86972c9f91185af01fdc5356299628d5c8de843d
John Beynon <john@render.com>
2022-11-07 12:07:30 +0000

    Add a JSON response

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 # myapp.rb
 require 'sinatra'
+require 'json'
 
 get '/' do
   puts "Hello"
+end
+
+not_found do
+  content_type :json
+  { statusCode:404,message:"Cannot GET /",error:"Not Found"}.to_json
 end
